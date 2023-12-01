5950a196cba669c77b95951755d3ac40236a86f3
Reid Luttrell <lut@pdx.edu>
2023-11-30 16:47:36 -0800

    made preprocess run on all files and added class identifier for target

--- preprocess.py
+++ preprocess.py
@@ -4,25 +4,40 @@ from matplotlib import pyplot as plt
 
 import os
 
-directory = os.fsencode("datasets/AbdomenCT")
+print(os.getcwd())
+write_file = open("datasets/ScanDataPreprocessed.txt", "a")
 
-write_file = open("datasets/PreprocessedAbdomenCT/data.txt", "a")
+directories = [
+    "datasets/AbdomenCT/",  # class id 1
+    "datasets/BreastMRI/",  # class id 2
+    "datasets/ChestCT/",  # class id 3
+    "datasets/CXR/",  # class id 4
+    "datasets/Hand/",  # class id 5
+    "datasets/HeadCT/",  # class id 6
+]
 
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    if filename.endswith(".jpeg"):
-        print(filename)
-        img = cv.imread("datasets/AbdomenCT/" + filename, cv.IMREAD_GRAYSCALE)
-        for x in range(0, img.shape[0]):
-            for y in range(0, img.shape[1]):
-                write_file.write(
-                    str(float(img[x, y]) / 255) + " "
-                )  # this is gonna put a trailing space at the end of the last feature
-                # print(img[x, y])
-        write_file.write(",")  # comma separated
-        # plt.imshow(img)
-        # plt.show()
-    else:
-        continue
+class_id = 1
+
+for directory in directories:
+    encoded_dir = os.fsencode(directory)
+    for file in os.listdir(encoded_dir):
+        filename = os.fsdecode(file)
+        if filename.endswith(".jpeg"):
+            print(filename)
+            img = cv.imread(directory + filename, cv.IMREAD_GRAYSCALE)
+            for x in range(0, img.shape[0]):
+                for y in range(0, img.shape[1]):
+                    write_file.write(
+                        str(float(img[x, y]) / 255) + " "
+                    )  # this is gonna put a trailing space at the end of the last feature
+            write_file.write(
+                class_id
+            )  # put class_id at the end, this will deal with trailing space
+            write_file.write(",")  # comma separated
+            # plt.imshow(img)
+            # plt.show()
+        else:
+            continue
+    class_id += 1  # new directory, so next class_id
 
 write_file.close()
