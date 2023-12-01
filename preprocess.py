aad578281ed43b552ea48dedf947ec428bcbd13b
Reid Luttrell <lut@pdx.edu>
2023-11-30 17:36:53 -0800

    preprocessor uses mnist format and only grabs 1000 imgs

--- preprocess.py
+++ preprocess.py
@@ -8,31 +8,36 @@ print(os.getcwd())
 write_file = open("../datasets/ScanDataPreprocessed.txt", "a")
 
 directories = [
-    "../datasets/AbdomenCT/",  # class id 1
-    "../datasets/BreastMRI/",  # class id 2
-    "../datasets/ChestCT/",  # class id 3
-    "../datasets/CXR/",  # class id 4
-    "../datasets/Hand/",  # class id 5
-    "../datasets/HeadCT/",  # class id 6
+    "../datasets/AbdomenCT/",  # class id 0
+    "../datasets/BreastMRI/",  # class id 1
+    "../datasets/ChestCT/",  # class id 2
+    "../datasets/CXR/",  # class id 3
+    "../datasets/Hand/",  # class id 4
+    "../datasets/HeadCT/",  # class id 5
 ]
 
-class_id = 1
+class_id = 0
 
 for directory in directories:
+    count = 0
+
+    print(directory)
     encoded_dir = os.fsencode(directory)
     for file in os.listdir(encoded_dir):
+        count += 1
+        if count >= 1000:
+            break
+
         filename = os.fsdecode(file)
         if filename.endswith(".jpeg"):
-            print(filename)
             img = cv.imread(directory + filename, cv.IMREAD_GRAYSCALE)
+
+            write_file.write(str(class_id))  # put class_id at beginning
+
             for x in range(0, img.shape[0]):
                 for y in range(0, img.shape[1]):
-                    write_file.write(
-                        str(float(img[x, y]) / 255) + " "
-                    )  # this is gonna put a trailing space at the end of the last feature
-            write_file.write(
-                str(class_id)
-            )  # put class_id at the end, this will deal with trailing space
+                    write_file.write(" " + str(float(img[x, y]) / 255))
+
             write_file.write(",")  # comma separated
             # plt.imshow(img)
             # plt.show()
